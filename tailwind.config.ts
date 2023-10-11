859a1ca6bbe4a0cd62f0170aa56e4aefc8361c46
DcWolf <danielmcolares@gmail.com>
2023-10-11 15:50:49 -0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+      'white': '#ffffff',
+      'gray': {
+        100: '#e1e1e6',
+        300: '#c4c4cc',
+        800: '#202024',
+        900: '#121214',
+      },
+      'green': {
+        300: '#00875f',
+        500: '#00b37e',
+      },
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
