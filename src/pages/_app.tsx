4c75d5acda2b15178bb0960a286f260a6ef08179
DcWolf <danielmcolares@gmail.com>
2023-10-11 17:23:02 -0300

    preparing header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Image from "next/image";
+import logoImage from "../assets/Logo.svg"
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-start">
+      <header className="py-8 w-full max-w-[1180px] mx-auto">
+        <Image alt="" src={logoImage} />
+      </header>
       <Component {...pageProps} />
     </div>
   );
