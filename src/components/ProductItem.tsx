6017942c60593978df8d3d21e087d68a44f020ca
DcWolf <danielmcolares@gmail.com>
2023-10-13 17:47:47 -0300

    ENV

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,8 +1,8 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
-import { FunctionComponent } from "react";
+import { FunctionComponent, HtmlHTMLAttributes } from "react";
 
-interface ProdutItemProps {
+interface ProdutItemProps extends HtmlHTMLAttributes<HTMLElement>{
   image: string | StaticImport;
   title: string;
   price: string;
@@ -12,9 +12,11 @@ export const ProductItem: FunctionComponent<ProdutItemProps> = ({
   image,
   price,
   title,
+  className,
+  ...rest
 }) => {
   return (
-    <div className="group relative rounded-xl bg-gradient-to-b from-blue-500 to-blue-300 overflow-hidden cursor-pointer">
+    <div className={`group ${className} relative rounded-xl bg-gradient-to-b from-blue-500 to-blue-300 overflow-hidden cursor-pointer` }{...rest}>
       <Image src={image} width={520} height={480} alt="" className="cover" />
       <footer className="absolute bottom-0 p-8 w-full flex flex-row justify-between items-center rounded-b-xl bg-gray-800 bg-opacity-60  transition-all duration-200  translate-y-[100%] group-hover:translate-y-[0%]">
         <strong className="text-lg font-bold"> {title}</strong>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 
-import product1 from"../assets/produtos/1.png"
-import product2 from"../assets/produtos/2.png"
-import product3 from"../assets/produtos/3.png"
+import product1 from "../assets/produtos/1.png";
+import product2 from "../assets/produtos/2.png";
+import product3 from "../assets/produtos/3.png";
 import { ProductItem } from "@/components/ProductItem";
-
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: { perView: 3, spacing: 48 },
+  });
   return (
     <main
-      className="flex gap-12 min-h-[36rem] w-full max-w-[calc(100vw-(100vw-1080px)/2)] ml-auto"
+      ref={sliderRef}
+      className="keen-slider flex flex-row min-h-[36rem] w-full max-w-[calc(100vw-(100vw-1080px)/2)] ml-auto"
     >
-      <ProductItem image={product1} price="79,99" title="Camisa1" />
-      <ProductItem image={product3} price="79,99" title="Camisa2" />
+      <ProductItem className={"keen-slider__slide"} image={product1} price="79,99" title="Camisa1" />
+      <ProductItem className={"keen-slider__slide"} image={product2} price="79,99" title="Camisa2" />
+      <ProductItem className={"keen-slider__slide"} image={product3} price="79,99" title="Camisa3" />
+      <ProductItem className={"keen-slider__slide"} image={product1} price="79,99" title="Camisa1" />
+      <ProductItem className={"keen-slider__slide"} image={product2} price="79,99" title="Camisa2" />
+      <ProductItem className={"keen-slider__slide"} image={product3} price="79,99" title="Camisa3" />
     </main>
-  )
+  );
 }
